c8745d5ee4957adf1bd972f6438f74a8360c28a1
Michael Fogleman <fogleman@gmail.com>
2014-03-23 20:15:34 -0400

    initial commit

---
+++ punchcard.py
@@ -0,0 +1,137 @@
+import csv
+import cairo
+import math
+
+PADDING = 12
+CELL_PADDING = 4
+MIN_SIZE = 4
+MAX_SIZE = 32
+MIN_COLOR = 0.8
+MAX_COLOR = 0.0
+
+FONT = 'Helvetica'
+FONT_SIZE = 14
+FONT_BOLD = False
+
+TITLE_FONT = 'Helvetica'
+TITLE_FONT_SIZE = 20
+TITLE_FONT_BOLD = True
+
+def set_font(dc, name=None, size=None, bold=False):
+    if name is not None:
+        weight = cairo.FONT_WEIGHT_BOLD if bold else cairo.FONT_WEIGHT_NORMAL
+        dc.select_font_face(name, cairo.FONT_SLANT_NORMAL, weight)
+    if size is not None:
+        dc.set_font_size(size)
+
+def punchcard(path, rows, cols, data, **kwargs):
+    # get options
+    padding = kwargs.get('padding', PADDING)
+    cell_padding = kwargs.get('cell_padding', CELL_PADDING)
+    min_size = kwargs.get('min_size', MIN_SIZE)
+    max_size = kwargs.get('max_size', MAX_SIZE)
+    min_color = kwargs.get('min_color', MIN_COLOR)
+    max_color = kwargs.get('max_color', MAX_COLOR)
+    title = kwargs.get('title', None)
+    font = kwargs.get('font', FONT)
+    font_size = kwargs.get('font_size', FONT_SIZE)
+    font_bold = kwargs.get('font_bold', FONT_BOLD)
+    title_font = kwargs.get('title_font', TITLE_FONT)
+    title_font_size = kwargs.get('title_font_size', TITLE_FONT_SIZE)
+    title_font_bold = kwargs.get('title_font_bold', TITLE_FONT_BOLD)
+    size = max_size + cell_padding * 2
+    # measure text
+    surface = cairo.ImageSurface(cairo.FORMAT_RGB24, 1, 1)
+    dc = cairo.Context(surface)
+    set_font(dc, font, font_size, font_bold)
+    row_text_size = max(dc.text_extents(str(x))[2] for x in rows)
+    col_text_size = max(dc.text_extents(str(x))[2] for x in cols)
+    # generate punchcard
+    width = size * len(cols) + row_text_size + padding * 3
+    height = size * len(rows) + col_text_size + padding * 3
+    if title is not None:
+        set_font(dc, title_font, title_font_size, title_font_bold)
+        title_size = dc.text_extents(title)[2:4]
+        height += title_size[1] + padding
+    dx = row_text_size + padding * 2
+    dy = col_text_size + padding * 2
+    surface = cairo.ImageSurface(cairo.FORMAT_RGB24, int(width), int(height))
+    dc = cairo.Context(surface)
+    set_font(dc, font, font_size, font_bold)
+    dc.set_source_rgb(1, 1, 1)
+    dc.paint()
+    dc.set_source_rgb(0, 0, 0)
+    dc.set_line_width(1)
+    # column labels
+    for i, col in enumerate(cols):
+        col = str(col)
+        tw, th = dc.text_extents(col)[2:4]
+        x = dx + i * size + size / 2 + th / 2
+        y = padding + col_text_size
+        dc.save()
+        dc.translate(x, y)
+        dc.rotate(-math.pi / 2)
+        dc.move_to(0, 0)
+        dc.show_text(col)
+        dc.restore()
+    # row labels
+    for j, row in enumerate(rows):
+        row = str(row)
+        tw, th = dc.text_extents(row)[2:4]
+        x = padding + row_text_size - tw
+        y = dy + j * size + size / 2 + th / 2
+        dc.move_to(x, y)
+        dc.show_text(row)
+    # grid
+    for i, col in enumerate(cols):
+        for j, row in enumerate(rows):
+            x = dx + i * size
+            y = dy + j * size
+            dc.rectangle(x, y, size, size)
+    dc.stroke()
+    # punches
+    lo = min(x for row in data for x in row if x)
+    hi = max(x for row in data for x in row if x)
+    for i, col in enumerate(cols):
+        for j, row in enumerate(rows):
+            value = data[j][i]
+            if not value:
+                continue
+            pct = 1.0 * (value - lo) / (hi - lo)
+            # pct = pct ** 0.5
+            radius = (pct * (max_size - min_size) + min_size) / 2.0
+            radius = int(round(radius))
+            color = pct * (max_color - min_color) + min_color
+            dc.set_source_rgb(color, color, color)
+            x = dx + i * size + size / 2
+            y = dy + j * size + size / 2
+            dc.arc(x, y, radius, 0, 2 * math.pi)
+            dc.fill()
+    # title
+    if title is not None:
+        dc.set_source_rgb(0, 0, 0)
+        set_font(dc, title_font, title_font_size, title_font_bold)
+        x = dx + size * len(cols) / 2 - title_size[0] / 2
+        y = height - padding
+        dc.move_to(x, y)
+        dc.show_text(title)
+    surface.write_to_png(path)
+
+def punchcard_from_csv(csv_path, path, **kwargs):
+    with open(csv_path, 'rb') as fp:
+        reader = csv.reader(fp)
+        rows = list(reader)
+    row_labels = [x[0] for x in rows[1:]]
+    col_labels = rows[0][1:]
+    data = [map(float, x[1:]) for x in rows[1:]]
+    punchcard(path, row_labels, col_labels, data, **kwargs)
+
+if __name__ == '__main__':
+    import sys
+    args = sys.argv[1:]
+    if len(args) == 2:
+        punchcard_from_csv(args[0], args[1])
+    elif len(args) == 3:
+        punchcard_from_csv(args[0], args[1], title=args[2])
+    else:
+        print 'Usage: python punchcard.py input.csv output.png [title]'
